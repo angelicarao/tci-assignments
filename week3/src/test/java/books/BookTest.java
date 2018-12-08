7df051657028ef56fddac7555b12090b144148a1
angelicarao <334307@student.fontys.nl>
2018-12-08 14:34:18 +0100

    Improved addToChapter test

--- week3/src/test/java/books/BookTest.java
+++ week3/src/test/java/books/BookTest.java
@@ -2,14 +2,8 @@ package books;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 public class BookTest {
 
@@ -18,38 +12,35 @@ public class BookTest {
     public final String CHAPTER_NAME = "Chapter";
     public final String CHAPTER_NR = "2.1";
 
+    public Book book = new Book(BOOK_NAME, BOOK_AUTHOR);
+
     @Test(expected = IllegalArgumentException.class)
     public void bookNameIsNotNull() {
-        Book book = new Book(null, BOOK_AUTHOR);
+        new Book(null, BOOK_AUTHOR);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void bookNameIsNotEmpty() {
-        Book book = new Book("", BOOK_AUTHOR);
+       new Book("", BOOK_AUTHOR);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void bookAuthorIsNotNull() {
-        Book book = new Book(BOOK_NAME, null);
+        new Book(BOOK_NAME, null);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void bookAuthorIsNotEmpty() {
-        Book book = new Book(BOOK_NAME, "");
+        new Book(BOOK_NAME, "");
     }
 
     @Test
-    public void addChapterToBook() {
-        Book book = mock(Book.class);
-        book.addChapter("name", "2.1");
-
-        verify(book).addChapter("name", "2.1");
+    public void addChapterToBookSucceeds() {
+        book.addChapter(CHAPTER_NAME, CHAPTER_NR);
     }
 
     @Test
     public void getTableOfContentsContainsChapter() {
-        Book book = new Book(BOOK_NAME, BOOK_AUTHOR);
-
         book.addChapter(CHAPTER_NAME, CHAPTER_NR);
 
         Assert.assertTrue(book.getTOC().contains(new Chapter(CHAPTER_NAME, CHAPTER_NR)));
